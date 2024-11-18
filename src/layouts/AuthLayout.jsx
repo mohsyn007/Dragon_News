c4ec8c32fc285c798a2d3dbad7d5a6bc2191120c
mohsyn007 <mohsinalam1141@gmail.com>
2024-11-18 22:41:27 +0600

    SetUp for authentication

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const AuthLayout = () => {
+    return (
+        <div>
+            Auth layout
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default AuthLayout;
